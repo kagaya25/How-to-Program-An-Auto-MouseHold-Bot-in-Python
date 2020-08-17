9f5cfebad582ae7ee954cec1c0e5532d736ae818
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-08-17 09:34:09 +0900

    Finish the Video
    finish the Video

--- mouseup.py
+++ mouseup.py
@@ -2,24 +2,17 @@ import pyautogui as pg
 import multiprocessing
 import time
 print(pg.position())
-
-def foo(n):
-   pg.moveTo(601,395)
-   pg.mouseDown(); pg.mouseUp()  # does the same thing as a left-button mouse click
+def foo():
+   pg.moveTo(427,356)
    pg.mouseDown(button='left')  # press the left button down
-    
-   
-   
 if __name__ == '__main__':
     # Start foo as a process
     p = multiprocessing.Process(target=foo)
     p.start()
-
     # Wait 69 seconds for foo
-    time.sleep(69.865)
-    pg.mouseUp(button='left', x=601, y=395)
+    time.sleep(6)
+    pg.mouseUp(button='left', x=427, y=356)
     # Terminate foo
     p.terminate()
-
     # Cleanup
     p.join()
